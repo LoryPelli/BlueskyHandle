900af9952591ffd783620ef56807b2ba615dfd66
lorypelli <lorypelli@proton.me>
2025-07-15 20:20:33 +0200

    use wildcard route to redirect

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono';
 const app = new Hono();
 const did = 'did:plc:jai46evw5qma2hfcrq7mxyjc';
 
-app.get('/', ({ redirect }) => redirect(`https://bsky.app/profile/${did}`));
 app.get('/.well-known/atproto-did', ({ text }) => text(did));
+app.get('*', ({ redirect }) => redirect(`https://bsky.app/profile/${did}`));
 
 export default app;
