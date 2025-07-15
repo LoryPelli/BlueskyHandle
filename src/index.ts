41c993454ee6ae56174b6fae0b5a119f8780b278
lorypelli <lorypelli@proton.me>
2025-07-15 20:16:42 +0200

    use `well-known` folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono';
 
 const app = new Hono();
+const did = 'did:plc:jai46evw5qma2hfcrq7mxyjc';
 
-app.get('/', ({ redirect }) =>
-    redirect('https://bsky.app/profile/did:plc:jai46evw5qma2hfcrq7mxyjc'),
-);
+app.get('/', ({ redirect }) => redirect(`https://bsky.app/profile/${did}`));
+app.get('/.well-known/atproto-did', ({ text }) => text(did));
 
 export default app;
