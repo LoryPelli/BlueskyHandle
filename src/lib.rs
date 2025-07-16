4a350e06007f38cc6a503e009c00ff9ec320e41c
lorypelli <lorypelli@proton.me>
2025-07-16 13:00:37 +0200

    value inside placeholder

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ const DID: &str = "did:plc:jai46evw5qma2hfcrq7mxyjc";
 #[event(fetch)]
 async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
     let is_well_known = req.url()?.path() == "/.well-known/atproto-did";
-    let url = format!("https://bsky.app/profile/{}", DID);
+    let url = format!("https://bsky.app/profile/{DID}");
     if is_well_known {
         Response::ok(DID)
     } else {
