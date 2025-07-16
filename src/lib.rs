1505ec572b3bc0183092dbd640058880ae888e11
lorypelli <lorypelli@proton.me>
2025-07-16 13:43:13 +0200

    moved url to the else block

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ const DID: &str = "did:plc:jai46evw5qma2hfcrq7mxyjc";
 #[event(fetch)]
 async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
     let is_well_known = req.url()?.path() == "/.well-known/atproto-did";
-    let url = format!("https://bsky.app/profile/{DID}");
     if is_well_known {
         Response::ok(DID)
     } else {
+        let url = format!("https://bsky.app/profile/{DID}");
         Response::redirect(Url::parse(&url)?)
     }
 }
