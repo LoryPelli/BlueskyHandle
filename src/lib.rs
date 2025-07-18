ba26b3bd1212d667ddb9b46cbaaf0e9100a1226c
lorypelli <lorypelli@proton.me>
2025-07-18 22:07:32 +0200

    don't create unnecessary `String`

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ const DH: &str = "dh=18180114862af9d7aacc108e3c62aa172ae5e904";
 
 #[event(fetch)]
 async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
-    let path = String::from(req.url()?.path());
+    let url = req.url()?;
+    let path = url.path();
     let well_known_uri = path.strip_prefix("/.well-known/");
     match well_known_uri {
         Some("atproto-did") => Response::ok(DID),
