6412f2c96929ad6d6c4ca59e4c0a377c306b49bc
lorypelli <lorypelli@proton.me>
2025-07-18 12:51:19 +0200

    added discord well-known

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,20 @@ use worker::{Context, Env, Request, Response, Result, Url, event};
 
 const DID: &str = "did:plc:jai46evw5qma2hfcrq7mxyjc";
 
+const DH: &str = "dh=18180114862af9d7aacc108e3c62aa172ae5e904";
+
 #[event(fetch)]
 async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
-    let is_well_known = req.url()?.path() == "/.well-known/atproto-did";
-    if is_well_known {
-        Response::ok(DID)
-    } else {
-        let url = format!("https://bsky.app/profile/{DID}");
-        Response::redirect(Url::parse(&url)?)
+    let path = String::from(req.url()?.path());
+    let well_known_uri = path.strip_prefix("/.well-known/");
+    match well_known_uri {
+        Some("atproto-did") => Response::ok(DID),
+        Some("discord") => Response::ok(DH),
+        Some(_) | None => redirect(),
     }
+}
+
+fn redirect() -> Result<Response> {
+    let url = format!("https://bsky.app/profile/{DID}");
+    Response::redirect(Url::parse(&url)?)
 }
