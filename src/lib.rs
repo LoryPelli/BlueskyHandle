ecb954d1ac1c174908d29cebc78ed02bd475c6f6
lorypelli <lorypelli@proton.me>
2025-07-19 09:07:09 +0200

    use macros instead of consts

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,29 @@
 use worker::{Context, Env, Request, Response, Result, Url, event};
 
-const DID: &str = "did:plc:jai46evw5qma2hfcrq7mxyjc";
+macro_rules! did {
+    () => {
+        "did:plc:jai46evw5qma2hfcrq7mxyjc"
+    };
+}
 
-const DH: &str = "dh=18180114862af9d7aacc108e3c62aa172ae5e904";
+macro_rules! dh {
+    () => {
+        "dh=18180114862af9d7aacc108e3c62aa172ae5e904"
+    };
+}
 
 #[event(fetch)]
 async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
     let url = req.url()?;
     let well_known_uri = url.path().strip_prefix("/.well-known/");
     match well_known_uri {
-        Some("atproto-did") => Response::ok(DID),
-        Some("discord") => Response::ok(DH),
+        Some("atproto-did") => Response::ok(did!()),
+        Some("discord") => Response::ok(dh!()),
         Some(_) | None => redirect(),
     }
 }
 
 fn redirect() -> Result<Response> {
-    let url = format!("https://bsky.app/profile/{DID}");
-    Response::redirect(Url::parse(&url)?)
+    let url = concat!("https://bsky.app/profile/", did!());
+    Response::redirect(Url::parse(url)?)
 }
