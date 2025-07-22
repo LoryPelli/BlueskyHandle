ad9d1465ad29d5722a748c0e76548f36382964a6
lorypelli <lorypelli@proton.me>
2025-07-22 14:27:22 +0200

    trait implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use worker::{Context, Env, Request, Response, Result, Url, event};
+use worker::{Context, Env, Request, Response, Result, event};
 
 macro_rules! prefix {
     ($suffix:expr) => {
@@ -18,10 +18,20 @@ macro_rules! dh {
     };
 }
 
-macro_rules! bsky_url {
+macro_rules! bsky {
     () => {
         concat!("https://bsky.app/profile/", did!())
     };
+}
+
+trait Redirect: Sized {
+    fn redirect_str(s: &str) -> Result<Self>;
+}
+
+impl Redirect for Response {
+    fn redirect_str(s: &str) -> Result<Self> {
+        Self::redirect(s.parse()?)
+    }
 }
 
 #[event(fetch)]
@@ -29,10 +39,6 @@ async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
     match req.path().as_str() {
         prefix!("atproto-did") => Response::ok(did!()),
         prefix!("discord") => Response::ok(dh!()),
-        _ => Response::redirect(to_url(bsky_url!())),
+        _ => Response::redirect_str(bsky!()),
     }
-}
-
-fn to_url(s: &str) -> Url {
-    s.parse().unwrap()
 }
