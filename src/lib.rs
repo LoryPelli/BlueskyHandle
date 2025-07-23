8ad67b1c4f3b0de41bac8b178a7a45c8dd316533
lorypelli <lorypelli@proton.me>
2025-07-23 09:35:37 +0200

    moved macros to separate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,14 @@
 use worker::{Context, Env, Request, Response, Result, event};
 
-macro_rules! prefix {
-    ($suffix:expr) => {
-        concat!("/.well-known/", $suffix)
-    };
-}
+mod macros;
 
-macro_rules! did {
-    () => {
-        "did:plc:jai46evw5qma2hfcrq7mxyjc"
-    };
-}
-
-macro_rules! dh {
-    () => {
-        "dh=18180114862af9d7aacc108e3c62aa172ae5e904"
-    };
-}
-
-macro_rules! bsky {
-    () => {
-        concat!("https://bsky.app/profile/", did!())
-    };
+#[event(fetch)]
+async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
+    match req.path().as_str() {
+        prefix!("atproto-did") => Response::ok(did!()),
+        prefix!("discord") => Response::ok(dh!()),
+        _ => Response::redirect_str(bsky!()),
+    }
 }
 
 trait Redirect: Sized {
@@ -31,14 +18,5 @@ trait Redirect: Sized {
 impl Redirect for Response {
     fn redirect_str(s: &str) -> Result<Self> {
         Self::redirect(s.parse()?)
-    }
-}
-
-#[event(fetch)]
-async fn fetch(req: Request, _: Env, _: Context) -> Result<Response> {
-    match req.path().as_str() {
-        prefix!("atproto-did") => Response::ok(did!()),
-        prefix!("discord") => Response::ok(dh!()),
-        _ => Response::redirect_str(bsky!()),
     }
 }
